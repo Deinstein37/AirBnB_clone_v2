a07b5c5faebf1ac5cd393bea6a7f2a133c9a5d76
Onolameh Daniel <101498178+Deinstein37@users.noreply.github.com>
2022-11-11 11:43:46 +0100

    Update 8-cities_by_states.py

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,68 +1,41 @@
 #!/usr/bin/python3
-"""
-starts a Flask web application
-"""
-
+""" Starts a Flash Web Application """
+from models import storage
+from models.state import State
+from os import environ
 from flask import Flask, render_template
-from models import *
-
 app = Flask(__name__)
+# app.jinja_env.trim_blocks = True
+# app.jinja_env.lstrip_blocks = True
 
 
-@app.route("/", strict_slashes=False)
-def index():
-    return "Hello HBNB!"
-
-
-@app.route("/hbnb", strict_slashes=False)
-def hbnb():
-    return "HBNB"
-
-
-@app.route("/c/<text>", strict_slashes=False)
-def C_is_fun(text):
-    return "C " + text.replace("_", " ")
-
-
-@app.route("/python", strict_slashes=False)
-def python():
-    return "Python is cool"
-
-
-@app.route("/python/<text>", strict_slashes=False)
-def Python_is_magic(text):
-    return "Python " + text.replace("_", " ")
-
-
-@app.route("/number/<int:n>", strict_slashes=False)
-def n_is_a_number(n):
-    return "{:d} is a number".format(n)
-
-
-@app.route('/number_template/<int:n>', strict_slashes=False)
-def number_template(n):
-    return render_template('5-number.html', num=n)
-
-@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
-def number_odd_or_even(n):
-    return render_template('6-number_odd_or_even.html', num=n)
+@app.teardown_appcontext
+def close_db(error):
+    """ Remove the current SQLAlchemy Session """
+    storage.close()
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    return render_template('7-states_list.html',
-                           states=storage.all("State"))
+    """ displays a HTML page with a list of states """
+    states = storage.all(State).values()
+    states = sorted(states, key=lambda k: k.name)
+    return render_template('7-states_list.html', states=states)
 
 
 @app.route('/cities_by_states', strict_slashes=False)
-def cities_by_states():
+def cities_list():
+    """ displays a HTML page with a list of cities by states """
+    states = storage.all(State).values()
+    states = sorted(states, key=lambda k: k.name)
+    st_ct = []
+    for state in states:
+        st_ct.append([state, sorted(state.cities, key=lambda k: k.name)])
     return render_template('8-cities_by_states.html',
-                           states=storage.all("State"))
+                           states=st_ct,
+                           h_1="States")
 
-
-@app.teardown_appcontext
-def teardown(err):
-    storage.close()
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port='5000')
+    """ Main Function """
+    app.run(host='0.0.0.0', port=5000)
