39edf956c1fdb50070676f1f656aafc8e08c73e9
Onolameh Daniel <101498178+Deinstein37@users.noreply.github.com>
2022-11-21 09:44:16 +0100

    Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,26 +1,22 @@
 #!/usr/bin/python3
-''' blueprint routes '''
-from api.v1.views import app_views
+"""Retrieves number for each type"""
 from flask import jsonify
 from models import storage
+from api.v1.views import app_views
 
 
-@app_views.route('/status', strict_slashes=False)
+@app_views.route("/status", strict_slashes=False)
 def status():
-    ''' return status '''
+    """Returns status"""
+    return jsonify({'status': 'OK'})
 
-    return (jsonify({"status": "OK"}))
 
-
-@app_views.route('/api/v1/stats')
-def show_count():
-    new_dict = {
+@app_views.route("/stats", strict_slashes=False)
+def stats():
+    """Returns stats"""
     return jsonify({"amenities": storage.count("Amenity"),
                     "cities": storage.count("City"),
                     "places": storage.count("Place"),
                     "reviews": storage.count("Review"),
                     "states": storage.count("State"),
-                    "users": storage.count("User")
-                    })
- }
-    return jsonify(new_dict)
+                    "users": storage.count("User")})
